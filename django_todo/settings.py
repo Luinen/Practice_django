573d5e170e6678122091479ff836a1c615fc4dd7
Luinen <lui.peto@gmail.com>
2023-04-18 15:04:58 +0000

    Procfile

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -1,10 +1,3 @@
-from pathlib import Path
-import os
-import dj_database_url
-
-if os.path.isfile("env.py"):
-    import env
-
 """
 Django settings for django_todo project.
 
@@ -18,6 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+if os.path.isfile("env.py"):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
